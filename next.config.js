83441f8309c54b0ac72c18591ec95e8df800171e
Domenico Ferraro <ferraro.domenico125@gmail.com>
2022-12-12 18:34:01 +0100

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone', // add support for Docker
-}
-
-module.exports = nextConfig
+})
